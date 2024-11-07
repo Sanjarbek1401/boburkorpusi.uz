7154597e9cdb5e0ea30a82db2bdfdb7a2c330e3a
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-11-07 21:07:27 +0500

    12

--- babur_corpus/settings.py
+++ babur_corpus/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'Bobur_corpus/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # A single directory for all collected static files
 
 STATICFILES_DIRS = [
