11e7d3ba7173d7ab53229b46c72b0bb795443349
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-11-07 19:20:20 +0500

    12

--- babur_corpus/urls.py
+++ babur_corpus/urls.py
@@ -52,5 +52,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings,STATIC_URL,document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings,STATIC_URL,document_root=settings.STATIC_ROOT)
