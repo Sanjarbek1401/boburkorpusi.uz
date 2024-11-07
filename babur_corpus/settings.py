9c67b52ef21fa59f3cb99065e7a12b788604fc8d
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-11-07 17:38:21 +0500

    1

--- babur_corpus/settings.py
+++ babur_corpus/settings.py
@@ -11,19 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6r0(5r)txbe1$&&@!e5#i*u(*($q*&8nyh#-v=0ei8ts2a6*=c'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     'https://40d9-194-93-25-162.ngrok-free.app'
@@ -81,15 +82,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'babur_corpus.wsgi.application'
 
+import os,dj_database_url
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
@@ -126,7 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- babur_corpus/urls.py
+++ babur_corpus/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+
+from babur_corpus.settings import STATIC_URL
 from main.views import (
     AuthorInfoAPIView,
     BaburnomaAPIView,
@@ -12,6 +14,8 @@ from main.views import (
     DivanTextAPIView,
     AdminContactAPIView
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Swagger documentation setup
 schema_view = get_schema_view(
@@ -48,3 +52,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings,STATIC_URL,document_root=settings.STATIC_ROOT)
