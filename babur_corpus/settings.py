d436da962360491605beefde2735aa71a9265b15
Sanjarbek1401 <159608407+Sanjarbek1401@users.noreply.github.com>
2024-11-09 16:45:56 +0500

    Update settings.py

--- babur_corpus/settings.py
+++ babur_corpus/settings.py
@@ -129,12 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # Add the path to drf-yasg static files explicitly
     os.path.join(BASE_DIR, 'static'),
 ]
 
