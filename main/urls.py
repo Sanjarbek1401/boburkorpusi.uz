6ea2231172f5ec64c8302d0466a99d4fd29d6f05
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-11-13 08:31:37 +0500

    123

--- main/urls.py
+++ main/urls.py
@@ -34,4 +34,8 @@ urlpatterns = [
     # Swagger va Redoc uchun
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
